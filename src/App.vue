593c1081cc51b28dce0c3e528349cf8c56d0ddb0
究极不粘锅 <1258161494@qq.com>
2022-08-16 18:12:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Author: 究极不粘锅 1258161494@qq.com
+ * @Date: 2022-08-16 09:13:05
+ * @LastEditors: 究极不粘锅 1258161494@qq.com
+ * @LastEditTime: 2022-08-16 13:25:41
+ * @FilePath: \vite-learn\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+a {
+    img {
+        border: 10px solid #333;
+    }
+}
+</style>
