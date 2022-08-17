82517426da44e769011cba01b482f2477cad9c1b
究极不粘锅 <1258161494@qq.com>
2022-08-17 19:58:13 +0800

    feat: add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+/*
+ * @Author: 究极不粘锅 1258161494@qq.com
+ * @Date: 2022-08-16 09:13:05
+ * @LastEditors: 究极不粘锅 1258161494@qq.com
+ * @LastEditTime: 2022-08-17 09:15:51
+ * @FilePath: \vue3-template\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
+import "tailwindcss/tailwind.css"
 import App from './App.vue'
 
 createApp(App).mount('#app')
